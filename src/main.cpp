eff3cc84ee1779bee141ee318a84f4ffd18c5628
Shengzhe Chen <shengzhe.chen@gmail.com>
2020-12-07 10:28:22 -0800

    input check

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
-#include "Config.h"
+#include <string>
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2) {
-    std::cout << argv[0] << " Version " << SC_CMAKE_VERSION_MAJOR << "." << SC_CMAKE_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+// Version Configuration
+//  if (argc < 2) {
+//    std::cout << argv[0] << " Version " << SC_CMAKE_VERSION_MAJOR << "." << SC_CMAKE_VERSION_MINOR << std::endl;
+//    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+//    return 1;
+//  }
+
+  if (argc > 0) {
+    const double inputValue = std::stod(argv[1]);
+    std::cout << inputValue << std::endl;
     return 1;
   }
 
-  return 0;
+  return -1;
 }
