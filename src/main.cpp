168bcfc93f0408be4e6c3211a051ef7194f6baf4
Shengzhe Chen <shengzhe.chen@gmail.com>
2020-12-07 08:20:21 -0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include "Config.h"
 
 int main(int argc, char *argv[])
 {
-  if (argc > 1) {
+  if (argc < 2) {
+    std::cout << argv[0] << " Version " << SC_CMAKE_VERSION_MAJOR << "." << SC_CMAKE_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
-  std::cout << "Hello World" << std::endl;
+
   return 0;
 }
