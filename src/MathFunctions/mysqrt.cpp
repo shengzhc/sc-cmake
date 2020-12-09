b42cde7ea2e77ae6fa2f2a4ab03693fe193a8e17
Shengzhe Chen <shengzhe.chen@gmail.com>
2020-12-09 08:20:04 -0800

    add check_symbol_exists

--- src/MathFunctions/mysqrt.cpp
+++ src/MathFunctions/mysqrt.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include "Config.h"
+
 double mysqrt(double inputValue)
 {
-  return inputValue + 1;
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = exp(log(inputValue) * 0.5);
+  std::cout << "Computing sqrt of " << inputValue << " to be " << result << " using log & exp" << std::endl;
+#else
+  double result = inputValue + 1;
+#endif
+  return result;
 }
